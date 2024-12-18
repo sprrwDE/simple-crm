e519e2d89d8b8e6d369c97884569be01c5df0c0f
Kai Schulz <info@sparrowmedia.de>
2024-12-18 09:20:43 +0100

    toolbar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatToolbarModule, MatSidenavModule, MatButtonModule],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'simple-crm';
-  isDarkTheme: boolean = false
+  title: string = 'simple-crm';
+  isDarkTheme: boolean = false;
+
+  constructor() {
+    this.toggleDarkTheme();
+  }
 
   toggleDarkTheme() {
     const body = document.body;
-    this.isDarkTheme = !this.isDarkTheme
+    this.isDarkTheme = !this.isDarkTheme;
     if (this.isDarkTheme) {
       body.classList.add('dark-theme');
     } else {
       body.classList.remove('dark-theme');
     }
   }
-
 }
