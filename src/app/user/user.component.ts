05456ec160879faaaea4c95342a796a87269390b
Kai Schulz <info@sparrowmedia.de>
2024-12-19 06:47:35 +0100

    added fab to add user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
