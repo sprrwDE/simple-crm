aaabb729bfd9b033afcc0622dd4429ec8d96af2f
Kai Schulz <info@sparrowmedia.de>
2024-12-22 07:59:08 +0100

    finally managed to get some data out of firebase db

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,11 +6,18 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddUserDialogComponent } from '../add-user-dialog/add-user-dialog.component';
 import { MatCardModule } from '@angular/material/card';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  onSnapshot,
+  doc,
+} from '@angular/fire/firestore';
 import { inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { query } from '@angular/fire/firestore';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -21,33 +28,45 @@ import { query } from '@angular/fire/firestore';
     MatTooltipModule,
     MatDialogModule,
     MatCardModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
   firestore = inject(Firestore);
-  users$: Observable<any>;
-  allUsers = [];
+  allUsers: User[] = [];
+  unsubList;
+  // unsubSingle;
 
   getUsers() {
-    return collection(this.firestore, 'users')
-    // const collectionRef = collection(this.firestore, 'users');
-    // return query(collectionRef);
+    return collection(this.firestore, 'users');
   }
 
   constructor(public dialog: MatDialog) {
-    this.users$ = collectionData(this.getUsers());
+    this.unsubList = onSnapshot(this.getUsers(), (list) => {
+      list.forEach((element) => {
+        const userData = element.data() as User; 
+        this.allUsers.push(userData);
+        console.log(this.allUsers);
+        // this.getSingleUser('users', element.id);
+      });
+    });
+
+    /* 
+    this.unsubSingle = onSnapshot(this.getSingleUser("users", "0TOAS3I6O16HqkGAqYc8"), (element) => {
+      list.forEach(list => {
+        console.log(list)
+      })
+    }) */
   }
 
-  ngOnInit() {
-    console.log(this.getUsers());
-    console.log(this.users$)
-    
-/* this.users$.subscribe((users) => {
-      console.log(users);
-    });  */
+  ngonDestroy() {
+    this.unsubList();
+  }
+
+  getSingleUser(colId: string, docId: string) {
+    console.log(doc(collection(this.firestore, colId), docId));
   }
 
   openDialog() {
