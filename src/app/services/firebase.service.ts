0e7deb38642f1a82f2ea5b8db80eaf4b0c0f19f7
Kai Schulz <info@sparrowmedia.de>
2024-12-25 14:31:09 +0100

    refactored dashboard with external firebase service

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
-import { User } from '../models/user.class';
+import { DocumentData } from 'firebase/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
-  unsubscribe;
-  allUsers: User[] = [];
+  private fetchedCollectionSubject = new BehaviorSubject<DocumentData[]>([]);
+  fetchedCollection$ = this.fetchedCollectionSubject.asObservable();
 
   constructor(public firestore: Firestore) {
-    try {
-      this.unsubscribe = onSnapshot(this.getUsers(), (list) => {
-        this.allUsers = [];
-        list.docs.forEach((element) => {
-          const rawData = element.data();
-          const user = new User({
-            ...rawData,
-            id: element.id,
-          });
-          this.allUsers.push(user);
-          // this.loaded = true;
-        });
-      });
-    } catch (error) {
-      console.error('error', error);
-    }
   }
 
-  getUsers() {
-    return collection(this.firestore, 'users');
+  getCollection(db: string) {
+    return collection(this.firestore, db);
+  }
+
+  getData(db: string) {
+    try {
+      onSnapshot(this.getCollection(db), (list) => {
+        const fetchedData: DocumentData[] = [];
+        list.docs.forEach((element) => {
+          const rawData = element.data();
+          fetchedData.push(rawData);
+        });
+        this.fetchedCollectionSubject.next(fetchedData);
+      });
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Daten:', error);
+    }
   }
 }
