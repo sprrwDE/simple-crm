7bc81b12b66e502602fe42e2ede9f6a2b8b11d26
Kai Schulz <info@sparrowmedia.de>
2024-12-30 17:53:15 +0100

    added firebase environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,16 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     MatNativeDateModule,
     provideNativeDateAdapter(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'simple-crm-28a05',
-        appId: '1:245225813153:web:176f9946d7fb922bb62ce0',
-        storageBucket: 'simple-crm-28a05.firebasestorage.app',
-        apiKey: 'AIzaSyAkAydBHzVSOUdyDG4yjvsUZHAP5TFss00',
-        authDomain: 'simple-crm-28a05.firebaseapp.com',
-        messagingSenderId: '245225813153',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
   ],
 };
